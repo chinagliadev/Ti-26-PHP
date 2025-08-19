d713880a89112947152cf519b82bdb670ec56516
Chinaglia <156460261+chinagliadev@users.noreply.github.com>
2025-08-19 11:49:16 -0300

    Continuacao da aula 02 - Crud

--- aula02_php/form-cadastro.php
+++ aula02_php/form-cadastro.php
@@ -9,31 +9,38 @@ $formUsuario        = $_POST['txtUsuario'];
 $formSenha          = $_POST['txtSenha'];
 $formConfirmarSenha = $_POST['txtConfirmarSenha'];
 
-$dsn      = 'mysql:dbname=db_cadastro;host=127.0.0.1';
-$usuario  = 'root';
-$password = '';
+if($formSenha != $formConfirmarSenha){
+    header('location:index.php?erro=sim');
+}else{
+    $dsn      = 'mysql:dbname=db_cadastro;host=127.0.0.1';
+    $usuario  = 'root';
+    $password = '';
+    
+    $conn = new PDO($dsn, $usuario, $password);
+    
+    $scriptCadastro = "INSERT INTO 
+        tb_cadastro (
+            nome, 
+            telefone, 
+            usuario, 
+            senha
+        ) 
+        VALUES (
+            :nome, 
+            :telefone, 
+            :usuario, 
+            :senha
+        )";
+    
+    $scriptPreparado = $conn->prepare($scriptCadastro);
+    
+    $scriptPreparado->execute([
+            ':nome' => $formNome,
+            ':telefone' => $formTelefone,
+            ':usuario' => $formUsuario,
+            ':senha' => $formSenha
+        ]);
+}
 
-$conn = new PDO($dsn, $usuario, $password);
 
-$scriptCadastro = "INSERT INTO 
-    tb_cadastro (
-        nome, 
-        telefone, 
-        usuario, 
-        senha
-    ) 
-    VALUES (
-        :nome, 
-        :telefone, 
-        :usuario, 
-        :senha
-    )";
-
-$scriptPreparado = $conn->prepare($scriptCadastro);
-
-$scriptPreparado->execute([
-        ':nome' => $formNome,
-        ':telefone' => $formTelefone,
-        ':usuario' => $formUsuario,
-        ':senha' => $formSenha
-    ]);
+?>
--- aula02_php/index.php
+++ aula02_php/index.php
@@ -1,5 +1,12 @@
 <?php
 include './template/header.php';
+
+if (isset($_GET['erro']) && $_GET['erro'] == 'sim') {
+    echo '<div class="alert alert-danger" role="alert">
+            A senha que você digitou não confere !
+          </div>';
+}
+
 ?>
 
 <main class="conteudo-principal container d-flex justify-content-center align-items-center">
@@ -8,23 +15,23 @@ include './template/header.php';
         <form action="./form-cadastro.php" method="POST">
             <div class="mb-3">
                 <label for="txtNome" class="form-label">Nome</label>
-                <input name="txtNome" type="text" class="form-control" id="txtNome" placeholder="">
+                <input name="txtNome" type="text" class="form-control" id="txtNome" placeholder="" required>
             </div>
             <div class="mb-3">
                 <label for="txtTelefone" class="form-label">Telefone</label>
-                <input name="txtTelefone" type="numeber" class="form-control" id="txtTelefone" placeholder="(xx) xxxxx-xxxx">
+                <input name="txtTelefone" type="numeber" class="form-control" id="txtTelefone" placeholder="(xx) xxxxx-xxxx" required>
             </div>
             <div class="mb-3">
                 <label for="txtUsuario" class="form-label">Usuario</label>
-                <input name="txtUsuario" type="text" class="form-control" id="txtUsuario" placeholder="">
+                <input name="txtUsuario" type="text" class="form-control" id="txtUsuario" placeholder="" required>
             </div>
             <div class="mb-3">
                 <label for="txtSenha" class="form-label">Senha</label>
-                <input name="txtSenha" type="password" class="form-control" id="txtSenha" placeholder="">
+                <input name="txtSenha" type="password" class="form-control" id="txtSenha" placeholder="" required>
             </div>
             <div class="mb-3">
                 <label for="txtConfirmarSenha" class="form-label">Confirmar Senha</label>
-                <input name="txtConfirmarSenha" type="password" class="form-control" id="txtConfirmarSenha" placeholder="">
+                <input name="txtConfirmarSenha" type="password" class="form-control" id="txtConfirmarSenha" placeholder="" required>
             </div>
             <div class="group-button d-flex justify-content-end">
                 <button class="btn btn-primary" type="submit">Enviar</button>
